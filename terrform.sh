20a3d9a728dd321733f4bf9673b499a81a096f4d
camario25 <camario25@yahoo.com>
2019-08-17 10:25:29 -0700

    terraform script to trouble shoot

--- terrform.sh
+++ terrform.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+
+sudo su
+apt-get -y update
+apt-get -y install python
+apt-get -y install python-pip
+curl -O https://releases.hashicorp.com/terraform/0.12.6/terraform_0.12.6_linux_amd64.zip
+apt-get -y install unzip
+mkdir /bin/terraform
+unzip terraform_0.12.6_linux_amd64.zip -d /bin/terraform
+export PATH=$PATH:/bin/terraform
+
+
