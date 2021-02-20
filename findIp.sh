6d9d11a6a37b761a83320280b3a440da7fa999dd
camario25 <camario25@yahoo.com>
2021-02-20 14:05:16 -0800

    add grepable nmap scan

--- findIp.sh
+++ findIp.sh
@@ -1,3 +1,6 @@
+#With temp file
 nmap -F 192.168.1.0/24 > temp_nmap_scan.txt
 cat temp_nmap_scan.txt | grep -i "Nmap scan report" > ipAddress.txt
 rm -f temp_nmap_scan.txt
+#Without temp file
+nmap -F -oG - 192.168.1.0/24 | grep "Host:" | sort -u >> ipAddress.txt
