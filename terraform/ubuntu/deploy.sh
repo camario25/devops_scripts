ab6923a0fffb35d5927279944e3dfd78cea13280
camario25 <camario25@yahoo.com>
2021-03-06 15:01:14 -0800

    add terraform script to decrypt keys and then remove decrypted file

--- terraform/ubuntu/deploy.sh
+++ terraform/ubuntu/deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-cp ~/secrets/terraform.tfvars.asc .
-gpg --decrypt --nosymkey-cache --output ./terraform.tfvars ./terraform.tfvars.asc
+# copy encrypted keys into this directory
+cp ../secrets/terraform.tfvars.asc .
+# use gnupg@1.4.23 to decrypt keys
+gpg1 --decrypt  --output ./terraform.tfvars ./terraform.tfvars.asc
+# confirm terraform is good
 terraform plan
+# apply terraform code
 terraform apply
+# wait a minute
 sleep 60s
+# remove the unencrypted keys file
 rm ./terraform.tfvars*
